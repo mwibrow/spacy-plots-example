b160a28be3234d234ad42b98d96ec77cb88ce029
Mark Wibrow <m.wibrow@gmail.com>
2020-12-21 14:12:38 +0000

    Update plotter

--- plotting/plotter.py
+++ plotting/plotter.py
@@ -2,7 +2,8 @@
 *Very* simple/opinionated plotter
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
-from typing import List
+# pylint: disable=unsubscriptable-object
+from typing import Dict, List, Union
 
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
@@ -12,18 +13,35 @@ plt.style.use('ggplot')
 
 class Plotter:
     
-    def __init__(self, ylabels: List[str] = None, title=None, iterations: int = 1, **kwargs):
-        self.ylabels = ylabels or ['']
+    """
+    lines = [
+        ["Train-loss", "Dev-loss"],
+        ["Precission", "Recall", "F-score"]
+    ]
+    """
+    def __init__(
+            self, 
+            plots: Union[List[str], Dict[str, List[str]]],
+            title: str = None, 
+            iterations: int = 1, 
+            **kwargs):
+        if isinstance(plots, list):
+            plots = {plot: [''] for plot in plots}
+        self.plots = plots 
+        self.ylabels = list(plots.keys())
+        self.nplots = len(self.plots)
+        
         self.iterations = iterations
-        self.nplots = len(self.ylabels)
-
         fig, axs = plt.subplots(nrows=self.nplots, ncols=1, sharex=True, **kwargs)
         fig.suptitle(title)
         self.fig = fig
         self.axs = np.atleast_2d(axs)
         
         self.x = []
-        self.y = [[] for _ in range(self.nplots)]
+        
+        self.y = {plot: [] for plot in self.plots}
+        for plot in self.plots:
+            self.y[plot].extend(([] for _ in self.plots[plot]))
         
         plt.subplots_adjust(hspace=0.1)
         plt.ion()
@@ -40,18 +58,25 @@ class Plotter:
 
     def plot(self):
         """Draw the plot."""
+
         for i, ax in enumerate(self.fig.axes):
             ax.clear()
             ax.set_xlim(1, self.iterations)
             ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-            ax.plot(self.x, self.y[i])
+
+            ylabel = self.ylabels[i]
+            plot = self.plots[ylabel]
+            for j, label in enumerate(plot):
+                ax.plot(self.x, self.y[ylabel][j], label=label)
+           
+            ax.legend()
             ax.set_ylabel(self.ylabels[i])
-        
+
         self.fig.axes[-1].set_xlabel('Iterations')
         self.fig.canvas.draw()
         plt.pause(0.1)
         
-    def update(self, y: List[float] = None, x: int = None):
+    def update(self, y: Union[List[float], Dict[str, List[float]]] = None, x: int = None):
         """Update the plot."""
         if x is not None:
             self.x.append(x)
@@ -61,9 +86,13 @@ class Plotter:
             else:
                 self.x = [1]
         if y is not None:
-            y = np.atleast_1d(y)
-            for i, val in enumerate(y):
-                self.y[i].append(val)
+            
+            if isinstance(y, list):
+                y = {self.ylabels[i]: y[i] for i in range(len(y))}
+
+            for i, ylabel in enumerate(self.ylabels):
+                for j in range(len(self.plots[ylabel])):
+                    self.y[ylabel][j].append(y[ylabel][j])
             
         self.plot()
 
@@ -71,4 +100,3 @@ class Plotter:
         """Keep the plot window open until it is closed."""
         while self.open:
             plt.pause(1.0)
-
